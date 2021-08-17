28b698d5a3796de820fe2f0f5a7031f0a2119058
ZettaDragon <89056052+ZettaDragon@users.noreply.github.com>
2021-08-16 22:41:34 -0600

    A ver si esta madre ya jala

--- app/src/main/java/com/example/electronicaws/MainActivity.java
+++ app/src/main/java/com/example/electronicaws/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+//Te la comes marcos
         //Esto es una simple prueba
     }
 }
