3bbf38622600ef96ec95f2e2a169fdb316d332a2
marcosonofremoreno <77844873+marcosonofremoreno@users.noreply.github.com>
2021-08-16 22:50:31 -0600

    asd

--- app/src/main/java/com/example/electronicaws/MainActivity.java
+++ app/src/main/java/com/example/electronicaws/MainActivity.java
@@ -5,13 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    public int ogt;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//Te la comes marcos
-        //Esto es una simple prueba
+
+
     }
 }
