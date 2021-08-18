d14119f3bb6a44bb07a16d221a2440e8aa02a880
marcosonofremoreno <77844873+marcosonofremoreno@users.noreply.github.com>
2021-08-17 23:07:35 -0600

    Clase Productos arreglada

--- app/src/main/java/com/example/electronicaws/Objetos/Productos.java
+++ app/src/main/java/com/example/electronicaws/Objetos/Productos.java
@@ -3,7 +3,6 @@ package com.example.electronicaws.Objetos;
 import java.io.Serializable;
 
 public class Productos implements Serializable {
-<<<<<<< HEAD
     private int _ID;
     private String marca;
     private String descripcion;
@@ -11,26 +10,10 @@ public class Productos implements Serializable {
 
     public Productos(){}
 
-    public Productos(String marca, String descripcion, String fotos)
-    {
+    public Productos(String marca, String descripcion, String fotos) {
         this.descripcion = descripcion;
         this.marca = marca;
         this.fotos = fotos;
-=======
-
-    private int _ID;
-    private String marca;
-    private String descripcion;
-    private String foto;
-
-    public Productos() {
-    }
-
-    public Productos(String marca, String descripcion, String foto) {
-        this.setMarca(marca);
-        this.setDescripcion(descripcion);
-        this.setFoto(foto);
->>>>>>> origin/main
     }
 
     public int get_ID() {
@@ -57,20 +40,13 @@ public class Productos implements Serializable {
         this.descripcion = descripcion;
     }
 
-<<<<<<< HEAD
+
     public String getFotos() {
         return fotos;
     }
 
     public void setFotos(String fotos) {
         this.fotos = fotos;
-=======
-    public String getFoto() {
-        return foto;
-    }
 
-    public void setFoto(String foto) {
-        this.foto = foto;
->>>>>>> origin/main
     }
 }
